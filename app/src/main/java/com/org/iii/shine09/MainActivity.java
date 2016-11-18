fbf19b963da20013965cc9e31683a45003f86ca8
user <a>
2016-11-18 13:55:21 +0800

    v4

--- app/src/main/java/com/org/iii/shine09/MainActivity.java
+++ app/src/main/java/com/org/iii/shine09/MainActivity.java
@@ -2,12 +2,31 @@ package com.org.iii.shine09;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.HashMap;
+import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView listView;
+    private SimpleAdapter adapter;
+    private LinkedList<HashMap<String,String>> data;
+    private String[]from = {"shine"};
+    private int[] to = {R.id.item_tv};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView)findViewById(R.id.listView);
+        initListview();
+    }
+    private void initListview(){
+        data = new LinkedList<>();
+        adapter = new SimpleAdapter(this,data,R.layout.layout_item,from,to);
+        listView.setAdapter(adapter);
     }
 }
